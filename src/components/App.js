a5c5407e9471b9218e8248f51b97f52c3b63f91e
Jordan Hiatt <jordan.hiatt11@gmail.com>
2018-05-30 15:58:08 -0400

    first

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class App extends Component {
+  render () {
+      return (
+        <div>
+          HI
+        </div>
+      );
+  }
+}
+
+//http://intljusticemission.github.io/react-big-calendar/examples/index.html
