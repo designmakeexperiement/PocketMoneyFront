a66e9be47f09a93b3489c9e3956b4b95c897e1ac
Jordan Hiatt <jordan.hiatt11@gmail.com>
2018-06-01 15:52:50 -0400

    information comming in view not working

--- src/containers/calendar.js
+++ src/containers/calendar.js
@@ -2,28 +2,34 @@ import React, {Component} from 'react';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 
+var array = [{date: '2016-01-13'}]
 
 BigCalendar.momentLocalizer(moment);
 
-const MyCalendar = props => (
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
+
+const Calendar = props => (
   <div>
     <BigCalendar
-      events={myEventsList}
-      startAccessor='startDate'
-      endAccessor='endDate'
+      events={array}
+      startAccessor='2016-01-01'
+      endAccessor='2016-01-31'
+      views={allViews}
     />
   </div>
 );
 
 
-export default class Calendar extends Component {
-  render () {
-      return (
-        <div>
-          <div>
-            <h1>sup</h1>
-          </div>       
-        </div>
-      );
-  }
-}
+// export default class Calendar extends Component {
+//   render () {
+//       return (
+//         <div>
+//           <div>
+//             <h1>sup</h1>
+//           </div>       
+//         </div>
+//       );
+//   }
+// }
+
+export default Calendar;
